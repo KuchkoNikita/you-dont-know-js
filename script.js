5994cc41332ec0bacd8445334a5241db78d89b4e
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-02-06 16:09:16 +0300

    Replacing the rear light

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* Убрал рекламу */
 let advRemoval = document.querySelector('.adv');
-console.log('advRemoval: ', advRemoval);
 advRemoval.style.display = 'none';
+
+/* Замена заднего фона */
+document.body.style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
