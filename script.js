481a340d1d195ea5ebf3b8634c9ef3af1e3474a0
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-02-06 16:30:15 +0300

    Reorder books

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ advRemoval.style.display = 'none';
 
 /* Замена заднего фона */
 document.body.style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
+
+/* Изменение последовательности книг */
+let book = document.querySelectorAll('.book'),
+    booksList = document.querySelector('.books');
+    console.log('book: ', book);
+
+booksList.insertBefore(book[1], book[0]);
+booksList.insertBefore(book[4], book[2]);
+booksList.insertBefore(book[3], book[2]);
+booksList.insertBefore(book[5], book[2]);
